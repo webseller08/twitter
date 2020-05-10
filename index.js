6b68f9bcc1bbb32ac812b204fa7a78b20b7a9fff
bcsf17m017 <bcsf17m017@pucit.edu.pk>
2020-05-10 18:11:48 +0500

    created login signup route,used static paths,and modified port selection

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 //requirements and imports
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 require("dotenv").config();
 
 var app = express();
 
 //midlewares
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "ui")));
+//routes
+app.use("/", require("./server/routes/loginSignup"));
 
 //app listening
 
-app.listen(3000, () => {
-  console.log("Server started listening at port : 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`server started at port ${port}`);
 });
